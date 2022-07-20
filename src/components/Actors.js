1f08063a750472e6d4f1f0e3b42ea4769b461eb6
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-07-20 23:44:08 +0300

    complete

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsItem=actors.map((actors)=>(
+    <div key={actors.name}>
+     <h2>{actors.name}</h2> 
+     <ul>
+      {
+        actors.movies.map((movies)=>(
+         <li key={movies}>
+          {movies}
+         </li> 
+        ))
+      }
+     </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Actors Page</h1>
+     {actorsItem}
+    </div>
+  )
 }
 
 export default Actors;
